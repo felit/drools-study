5aa53c79ccab3b5058a7c41694859e337b0f751a
felit <nofelit@126.com>
2014-12-11 15:36:46 +0800

    add task statements

--- chapter03/src/main/java/com/felit/drools/chapter03/task/TaskClientMain.java
+++ chapter03/src/main/java/com/felit/drools/chapter03/task/TaskClientMain.java
@@ -18,17 +18,16 @@ public class TaskClientMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.jbpm.persistence.jpa");
         TaskService taskService = new TaskService(emf, SystemEventListenerFactory.getSystemEventListener());
         TaskServiceSession tsession = taskService.createSession();
+
         List<TaskSummary> tasks = tsession.getTasksAssignedAsPotentialOwner("jbpm-test2", "en-UK");
 //        tasks = tsession.getTasksAssignedAsBusinessAdministrator("Administrator", "en-UK");
         LocalTaskService localTaskService = new LocalTaskService(taskService);
 
         for (TaskSummary taskSummary : tasks) {
             System.out.println(taskSummary);
-//            localTaskService.(taskSummary.getId(), "jbpm-test2");
 //            localTaskService.start(taskSummary.getId(), "jbpm-test2");
 //            localTaskService.claim(taskSummary.getId(), "jbpm-test2");
             localTaskService.complete(taskSummary.getId(), "jbpm-test2",null);
-
 //            localTaskService.delegate(taskSummary.getId(),"Administrator","jbpm-test2");
         }
     }
